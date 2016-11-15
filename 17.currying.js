242eb6af0fcfff808b30daecd10cb2be91319497
Piotr Karpala <pkarpala@infusion.com>
2016-11-14 22:59:16 -0500

    currying verified - but without bind

--- 17.currying.js
+++ 17.currying.js
@@ -1,31 +1,28 @@
 /*jshint esversion: 6, node: true*/
 function curryN(fn, n, args) {
-    n = n || fn.length;
-    args = args || [];
-
-    if (n == 1) {
-        return function() {
-            args = args.concat(Array.prototype.slice.call(arguments));
-            return fn.apply(this, args);
-        };
-    }
+  n =  n || fn.length;
+  args = (args || []);
 
     return function() {
-        args.push(arguments[0]);
-        return curryN(fn, --n, args);
+        let localArgs = args.concat(arguments[0]);
+
+        if(n == 1) {
+          return fn.apply(this, localArgs);
+        }
+        return curryN(fn, n-1, localArgs);
     };
 }
 
 module.exports = curryN;
 
-function add3(one, two, three) {
-    return one + two + three;
-}
-
+// function add3(one, two, three) {
+//   return one + two + three;
+// }
+//
 // var curryC = curryN(add3);
 // var curryB = curryC(1);
 // var curryA = curryB(2);
 // console.log(curryA(3)); // => 6
-//console.log(curryA(10)); // => 13
-
-console.log(curryN(add3)(1)(2)(3)); // => 6
+// console.log(curryA(10)); // => 13
+//
+// console.log(curryN(add3)(1)(2)(3)); // => 6
